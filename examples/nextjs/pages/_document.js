b3523ac47097487a398e07a41bc24c22c236d4f3
Ryan MacArthur <ryan.macarthur@gmail.com>
2023-03-29 10:06:06 -0400

    Update _document.js

--- examples/nextjs/pages/_document.js
+++ examples/nextjs/pages/_document.js
@@ -1,13 +1,31 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+
+export default class MyDocument extends Document {
+  render(): JSX.Element {
+    return (
+      <Html lang="en">
+        <Head>
+        <meta name="apple-mobile-web-app-title" content="appbase.ai: ai powered widgets"/>
+        <meta name="application-name" content="appbase.ai: ai powered widgets" />
+        <link
+           href="https://cdn.appbase.ai/widget/index.css"
+           rel="stylesheet"
+        />
+        <script
+           id="appbase-widget"
+           data-deploymentKey="497683dd476b4773928be41a19a3e146"
+           defer
+           type="module"
+           src="https://cdn.appbase.ai/widget/index.js"
+        />
+
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
