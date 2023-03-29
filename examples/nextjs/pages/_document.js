f249026b8ec3e0d7abc682ae4cdd6ce8926f986b
Ryan MacArthur <ryan.macarthur@gmail.com>
2023-03-29 10:15:57 -0400

    Update _document.js

--- examples/nextjs/pages/_document.js
+++ examples/nextjs/pages/_document.js
@@ -1,31 +1,29 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 
 
-export default class MyDocument extends Document {
-  render(): JSX.Element {
+export default function Document() {
     return (
-      <Html lang="en">
-        <Head>
-        <meta name="apple-mobile-web-app-title" content="appbase.ai: ai powered widgets"/>
-        <meta name="application-name" content="appbase.ai: ai powered widgets" />
-        <link
-           href="https://cdn.appbase.ai/widget/index.css"
-           rel="stylesheet"
-        />
-        <script
-           id="appbase-widget"
-           data-deploymentKey="497683dd476b4773928be41a19a3e146"
-           defer
-           type="module"
-           src="https://cdn.appbase.ai/widget/index.js"
-        />
+        <Html lang="en">
+            <Head>
+                <meta name="apple-mobile-web-app-title" content="appbase.ai: ai powered chatgpt4 plugin widgets" />
+                <meta name="application-name" content="appbase.ai: ai powered gpt4 plugin widgets" />
+                <link
+                    href="https://cdn.appbase.ai/widget/index.css"
+                    rel="stylesheet"
+                />
+                <script
+                    id="appbase-widget"
+                    data-deploymentKey="497683dd476b4773928be41a19a3e146"
+                    defer
+                    type="module"
+                    src="https://cdn.appbase.ai/widget/index.js"
+                />
 
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
     )
-  }
 }
